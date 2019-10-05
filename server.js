147967a7ed365f89b223ce7323ddaac600eac98c
userJapa <willian.hsimabukuro@gmail.com>
2019-10-05 20:53:19 -0300

    exemplos inseridos

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-const os = require('os')
+const express    = require('express'),
+      path       = require('path'),
+      bodyParser = require('body-parser')
 
-console.log(os.cpus().length)
+const app = express()
+
+let count = 1
+
+app.use(bodyParser.json())
+
+app.post('/api/request', (req, res) => {
+  console.log(`IP: ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
+  console.log(req.body)
+  res.json({ test: `ola do servidor | ${count} x` })
+  count++
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.listen(8000, () => console.log('Rodando na porta 8000'))
